485a61d4e9e04483ee0652188dcb64551ad472a6
adamGrgic <argrgic@gmail.com>
2025-01-03 13:10:35 -0700

    JS Hard Parts V2: Higher order functions and callbacks.

---
+++ FrontEndMasters/JSTheHardPartsV2/index.js
@@ -0,0 +1,34 @@
+// use this file for testing various concepts within the course
+//
+
+//console.log('initializing front end masters file')
+//
+//console.log('this is the thread of execution module')
+//console.log('javascript executes content line by line')
+//console.log('line 1')
+//console.log('line 2')
+//console.log('line 3')
+
+
+//
+//
+//
+// ..
+// Generalized Functions
+
+// for later / another file -- wanted to see if I could see memory being used at
+// run time in nodejs
+//const memoryUsage = process.memoryUsage();
+//console.log('Memory Usage:', {
+//    rss: memoryUsage.rss,       // Resident Set Size
+//    heapTotal: memoryUsage.heapTotal,
+//    heapUsed: memoryUsage.heapUsed,
+//    external: memoryUsage.external,
+//});
+//
+// end
+
+
+// Repeating functionality
+
+
